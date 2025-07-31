7c0d347b7e72d5bdaa727a89e4220a963b905f3b
IanPaniagua <paniagua.ian.de@gmail.com>
2025-07-31 13:00:43 +0200

    Use multiple println statements to print a sentence.
    Closes #8

--- src/MyName.java
+++ src/MyName.java
@@ -4,13 +4,21 @@ public class MyName {
     public static void main (String[] args)
     {
         //this line print some text in a line
-        System.out.println("Ian Manuel Paniagua Porroa");
+        System.out.println("Ian Manuel Paniagua Porroa\n");
 
         //Print your age, favorite color, and city.
         System.out.println(("Age: 31\nFavorite Color: Blue\nCity: Mallorca"));
 
         // 7 Explore the different System.XXX.println(); options beyond out.
         System.err.println("This is to print an error, it will appear in red");
+
+        // 8 Use multiple println statements to print a sentence.
+        System.out.println("\nMultiple ");
+        System.out.println("println ");
+        System.out.println("statments ");
+        System.out.println("to print a ");
+        System.out.println("sentence.\n");
+
 
     }
 }
