b740c15ae1bc207333b193a2ba5dcdcd7e22ca74
IanPaniagua <paniagua.ian.de@gmail.com>
2025-07-31 12:38:48 +0200

    Add a comment about what each line of the program does. Closes #4

--- src/MyName.java
+++ src/MyName.java
@@ -1,7 +1,9 @@
+//this line define the class
 public class MyName {
+    //this line define the main
     public static void main (String[] args)
     {
+        //this line print some text in a line
         System.out.println("Ian Manuel Paniagua Porroa");
     }
 }
-//Testing Github Issus to close it
