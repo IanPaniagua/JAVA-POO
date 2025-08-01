097122dc60218c68b561ee37f08c40b9557248d9
IanPaniagua <paniagua.ian.de@gmail.com>
2025-08-01 08:04:55 +0200

    Data Types, primitive and clases

---
+++ src/DataTypes.java
@@ -0,0 +1,26 @@
+public class DataTypes {
+    public static void main(String[] args){
+
+        //Tipo de datos primitvos
+
+        int myInt = 12;
+
+        double myDouble = 12.12;
+        // float, long, byte
+
+        char myChar = 'a';
+
+        boolean myBoolean = true;
+
+        // Consideramos String primitivo aunque no lo es;
+
+        String myString = "IAN";
+
+        // Un dato primitvo puede ser utilizado como clase poniendo la primera letra en mayuscula
+
+        Boolean myBooleanClass = false;
+
+
+
+    }
+}
