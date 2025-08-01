356433b79e00d824462aa2bba966132dd2594aba
IanPaniagua <paniagua.ian.de@gmail.com>
2025-08-01 08:15:46 +0200

    // Declare a String variable and assign your name to it. Closes #13

---
+++ src/Exercises/VariablesAndConstants.java
@@ -0,0 +1,12 @@
+package Exercises;
+
+public class VariablesAndConstants {
+    public static void main(String[] args){
+        // Declare a String variable and assign your name to it. #13
+        String myName = "IAN";
+
+
+
+
+    }
+}
